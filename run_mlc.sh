b94cd1350920ea793bfa8fce8e5d74d77ddf08b8
lei <lei.a.zhang@intel.com>
2019-01-17 09:27:45 +0000

    Init repo
    Add some scripts for running mlc and collecting stats

---
+++ run_mlc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo -E tee ./ansible/servers.yml >/dev/null << EOF
+[servers]
+EOF
+
+source /etc/kolla/admin-openrc.sh
+
+openstack server list -f value -c Networks | awk -F"=" '{print $2}' >> ./ansible/servers.yml
+
+#ip netns exec qrouter-f210cff1-ae19-4e55-8e4b-691be4dd18eb ansible-playbook -i ./ansible/servers.yml ./ansible/benchmark.yml --extra-vars "ansible_python_interpreter=/usr/bin/python3"
+
